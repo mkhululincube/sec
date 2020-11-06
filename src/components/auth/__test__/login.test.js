51179d49e97484158138f1bd9790ec7e7304ef2f
Mkhululi Ncube <mkayz89@egmail.com>
2020-11-07 03:28:42 +0400

    tested fail for login form

---
+++ src/components/auth/__test__/login.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { Login } from '../login';
+
+describe('Login form', ()=>{
+    test ('validate should pass correct input', ()=>{
+        const username="mkhululi";
+        //expect()
+    })
+})
